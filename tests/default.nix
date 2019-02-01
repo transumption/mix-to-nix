1c8a842627a2e8db5e3d2c12ca679efff7bfbcee
Yegor Timoshenko <yegortimoshenko@riseup.net>
2019-02-01 21:00:54 +0300

    Add Pleroma test

--- tests/default.nix
+++ tests/default.nix
@@ -31,4 +31,48 @@ in
   };
 
   _03-cowboy = mixToNix { src = ./03-cowboy; };
+
+  _04-pleroma = let
+    # Fake config, required for compilation
+    prodSecret = writeText "prod.secret.exs" ''
+      use Mix.Config
+
+      config :pleroma, Pleroma.Repo,
+        adapter: Ecto.Adapters.Postgres
+    '';
+  in (mixToNix {
+    src = stdenv.mkDerivation {
+      name = "pleroma";
+      src = fetchGit {
+        url = https://git.pleroma.social/pleroma/pleroma;
+        rev = "3879500c87829a5cf1377ca7ccdb7bf92c75367f";
+      };
+
+      buildCommand = ''
+        cp --no-preserve=mode -r $src $out
+
+        # Suppress runtime git invocation
+        substituteInPlace $out/mix.exs \
+          --replace 'version: version' 'version: to_string'
+
+        # Fork with v1.4 as HEAD branch, see:
+        # https://github.com/NixOS/nix/pull/2409
+        substituteInPlace $out/mix.lock \
+          --replace phoenixframework/phoenix yegortimoshenko/phoenix
+
+        cp ${prodSecret} $out/config/prod.secret.exs
+      '';
+    };
+
+    overlay = final: previous: {
+      cowlib = previous.cowlib.overrideAttrs (super: {
+        postPatch = ''
+          substituteInPlace src/cow_multipart.erl \
+            --replace crypto:rand_bytes crypto:strong_rand_bytes
+        '';
+      });
+    };
+  }).overrideAttrs (super: {
+    doCheck = false;
+  });
 }
